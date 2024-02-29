539b965cda8e1ee433e8ed628794e07d244e2456
Sebastian Drela <sebastian.drela@wp.pl>
2024-02-29 03:24:20 +0100

    add base for inter process communication

---
+++ PlayerOne/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.IO.Pipes;
+
+namespace PlayerOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SetUpPlayer();
+        }
+
+        private static void SetUpPlayer()
+        {
+            using var namedPipeServerStream = new NamedPipeServerStream("PlayerOne");
+            using var namedPipeClientStream = new NamedPipeClientStream("PlayerTwo");
+
+            Console.WriteLine($"Waiting for PlayerTwo...");
+
+            namedPipeClientStream.Connect();
+            namedPipeServerStream.WaitForConnection();
+            Console.WriteLine("Connected.");
+
+            using var streamReader = new StreamReader(namedPipeClientStream);
+            using var streamWriter = new StreamWriter(namedPipeServerStream);
+
+            while (true)
+            {                
+                var message = streamReader.ReadLine();
+                Thread.Sleep(1000);
+
+                Console.WriteLine($"{message}");
+
+                streamWriter.WriteLine("Response 2");
+                streamWriter.Flush();               
+            }
+        }
+    }
+}
---
+++ PlayerTwo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.IO.Pipes;
+using System.Threading.Channels;
+
+namespace PlayerTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SetUpPlayer();
+        }
+
+        private static void SetUpPlayer()
+        {
+            using var namedPipeServerStream = new NamedPipeServerStream("PlayerTwo");
+            using var namedPipeClientStream = new NamedPipeClientStream("PlayerOne");
+
+            Console.WriteLine($"Waiting for PlayerOne...");
+
+             namedPipeClientStream.Connect();
+             namedPipeServerStream.WaitForConnection();
+
+            Console.WriteLine("Connected.");
+
+            using var streamReader = new StreamReader(namedPipeClientStream);
+            using var streamWriter = new StreamWriter(namedPipeServerStream);
+
+            while (true)
+            {               
+                streamWriter.WriteLine("Response 1");
+                streamWriter.Flush();
+               
+                var message = streamReader.ReadLine();
+                Thread.Sleep(1000);
+                Console.WriteLine($"{message}");              
+            }
+        }
+    }
+}
