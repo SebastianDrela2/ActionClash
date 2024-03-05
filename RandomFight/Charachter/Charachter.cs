f8ebf29ade8932a12754f1fffbe4d1c7d249b11f
Sebastian Drela <sebastian.drela@wp.pl>
2024-03-05 01:51:31 +0100

    tweak possible armor and the hp

--- RandomFight/Charachter/Charachter.cs
+++ RandomFight/Charachter/Charachter.cs
@@ -9,8 +9,8 @@
         {
             var random = new Random();
 
-            HealthPoints = random.Next(1, 100);                       
-            Armor = random.Next(1, HealthPoints/4);
+            HealthPoints = random.Next(1, 1000);                       
+            Armor = random.Next(1, HealthPoints/8);
         }
     }
 }
